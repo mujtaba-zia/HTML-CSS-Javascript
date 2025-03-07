e725913e44c75e7ea605a1321fd6b51ee8302f9c
mujtaba-zia <mujtabayousufzai786@gmail.com>
2025-03-07 12:24:46 -0700

    Added lab6 src files

---
+++ lab6/exercise1/src/App.js
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+function App() {  
+  const currentYear = new Date().getFullYear();
+  
+  const isLoggedIn = true;
+  return (
+    <div>
+      
+      <h1>ENSF-381: Full Stack Web Development</h1>
+      <p>React Components</p>
+      <p>Current Year: {currentYear}</p>
+      <p>{isLoggedIn ? "Welcome back!" : "Please log in."}</p>
+    </div>
+  );
+}
+
+export default App;
